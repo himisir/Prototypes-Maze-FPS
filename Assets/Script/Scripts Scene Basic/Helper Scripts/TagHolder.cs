f10491def1b0c2379b7fdc12c78c017b720a988c
himisir <himisirhimu@gmail.com>
2022-08-21 00:41:16 +0600

    UP

---
+++ Assets/Script/Scripts Scene Basic/Helper Scripts/TagHolder.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axis
+{
+    public const string HORIZONTAL = "Horizontal";
+    public const string VERTICAL = "Vertical";
+}
+
+public class MouseAxis
+{
+    public const string MOUSE_X = "Mouse X";
+    public const string MOUSE_Y = "Mouse Y";
+    public const string MOUSE_WHEEL = "Mouse ScrollWheel";
+    public const string MOUSE_LEFT = "Mouse 0";
+    public const string MOUSE_RIGHT = "Mouse 1";
+}
+
+public class AnimationTags
+{
+    public const string SHOOT = "isShooting";
+    public const string RELOAD = "isReloading";
+    public const string AIM = "isAiming";
+    public const string ZOOM_IN = "isZoomingIn";
+    public const string ZOOM_OUT = "isZoomingOut";
+}
